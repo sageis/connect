ad0292d3ba5e45ef54b5d729afca6d88030190a6
JasmineG <yo.kitchen@gmail.com>
2015-11-13 11:41:55 -0800

    updating

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,8 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-
+Rails.application.config.assets.precompile += %w( my-custom-theme.min.css )
+Rails.application.config.assets.precompile += %w( jquery.mobile.icons.min.css )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
