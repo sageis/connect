887d746b68e47aeef5942ac9f8cb1dc9b796c14d
JasmineG <yo.kitchen@gmail.com>
2015-11-13 11:00:06 -0800

    some stuff

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,12 @@ Rails.application.routes.draw do
   get 'users/index'
 
   get 'programs/index'
-root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
-  get 'users' => 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
