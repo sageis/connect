121d930e610730b619fb7d6c15694202ac9deabe
JasmineG <yo.kitchen@gmail.com>
2015-11-18 10:59:07 -0800

    new stuff

--- app/controllers/professions_controller.rb
+++ app/controllers/professions_controller.rb
@@ -5,7 +5,24 @@ class ProfessionsController < ApplicationController
 
 	def quiz
 		@all_professions = Profession.all
+		@new_quiz = Quiz.new
+		@new_quiz.save
+	end
 
+	def create 
+		# raise params.inspect
+		puts params.inspect
+		@new_quiz = Quiz.find_by_id(params[:id])
+		# @new_quiz.file_name = params[:]
+		@new_quiz.writing = params[:profession][:writing]
+		if (@user.save) then
+			redirect_to(:controller => "professions", :action => "index")
+		end
+
+	end
+
+	def results
+		@all_professions = Profession.all
 	end
 
 	def analyze_results
