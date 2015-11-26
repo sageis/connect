5f5a4433ee91c0696fb6cace86d287860bca5db5
JasmineG <yo.kitchen@gmail.com>
2015-11-25 17:22:10 -0800

    hfuishfudis

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -2,4 +2,11 @@ class ProgramsController < ApplicationController
 	def index
 		@all_programs = Program.all
 	end
+
+	def search
+		@all_programs = Program.all
+		@program = Program.new
+		#professions_list = Array.new(num_professions)
+	end
+
 end
